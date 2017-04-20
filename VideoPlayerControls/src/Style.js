94398c898636ef089668687efa906c74cf7e2cf4
Do Tri <tri@ezyplanet.com>
2017-04-20 16:45:52 +0700

    add play + pause buttons

--- VideoPlayerControls/src/Style.js
+++ VideoPlayerControls/src/Style.js
@@ -2,12 +2,54 @@ import { StyleSheet } from 'react-native';
 
 var Style = StyleSheet.create({
     backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    height: 100,
-    right: 0,
-  }
+   
+  },
+  mainContainer:{
+        flex:1                  //Step 1
+  },
+  content:{
+        flex:1 ,
+        flexDirection: 'column'
+    },
+  controls: {
+     flexDirection: 'row',
+    backgroundColor: '#2196F3',
+    height: 50,
+    justifyContent: 'center',
+        alignItems: 'center',
+  },
+  backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+    },
+  playButton: {
+      width: 30,
+      height: 37
+  },
+  playImage: {
+      width: 23,
+      height: 28,
+
+  },
+  toolbar:{
+        backgroundColor:'#81c04d',
+        paddingTop:35,
+        paddingBottom:10,
+        flexDirection:'row'    //Step 1
+    },
+    toolbarButton:{
+        width: 50,            //Step 2
+        color:'#fff',
+        textAlign:'center'
+    },
+    toolbarTitle:{
+        color:'#fff',
+        textAlign:'center',
+        fontWeight:'bold',
+        flex:1                //Step 3
+    }
 });
 
 export default Style;
--- VideoPlayerControls/src/VideoController.js
+++ VideoPlayerControls/src/VideoController.js
@@ -1,32 +1,83 @@
 import React, {Component} from "react";
-import {AppRegistry, StyleSheet, Text, TouchableOpacity, View, Dimensions} from "react-native";
+import {AppRegistry, StyleSheet, Text, TouchableOpacity, View, Dimensions,Image, TouchableHighlight} from "react-native";
 import Style from './Style';
 import Video from 'react-native-video';
 
 let width = Dimensions.get('window').width;
 let height = width / 1280.0 * 720
+const PlayIcon = require('../assets/images/play@3x.png');
+const PauseIcon = require('../assets/images/pause@3x.png');
+let isAutoPlay = false
+
+class BackgroundImage extends Component {
+    render() {
+        return (
+            <Image source={require('../assets/images/bg@2x.png')}
+                  style={Style.backgroundImage}>
+                  {this.props.children}
+            </Image>
+        )
+    }
+}
 
 class VideoController extends Component {
   player: Video;
-
+  constructor(props) {
+        super(props);
+        this.state = { 
+            autoplay: isAutoPlay,
+            paused: true,
+            playpause_imageuri: isAutoPlay ? PauseIcon : PlayIcon
+        };
+    }
   render() {
        return (
-        <Video
-        source={require('../broadchurch.mp4')}   // Can be a URL or a local file.
-       ref={(ref) => {
-         this.player = ref
-       }}                                      // Store reference
-       rate={1.0}                              // 0 is paused, 1 is normal.
-       volume={1.0}                            // 0 is muted, 1 is normal.
-       muted={false}                           // Mutes the audio entirely.
-       paused={false}                          // Pauses playback entirely.
-       resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
-       repeat={true}                           // Repeat forever.
-       playInBackground={false}                // Audio continues to play when app entering background.
-       playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
-       progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
-       style={[Style.backgroundVideo, {width: width, height: height}]} />
+         <BackgroundImage>
+            <View style={Style.mainContainer}>
+                  <View style={Style.toolbar}>
+                      <Text style={Style.toolbarButton}>Add</Text>
+                      <Text style={Style.toolbarTitle}>This is the title</Text>
+                      <Text style={Style.toolbarButton}>Like</Text>
+                  </View>
+                <View style={Style.content}>
+                <Video
+                      source={require('../broadchurch.mp4')}   // Can be a URL or a local file.
+                    ref={(ref) => {
+                      this.player = ref
+                    }}                                      // Store reference
+                    rate={this.state.autoplay ? 1 : 0}                              // 0 is paused, 1 is normal.
+                    volume={1.0}                            // 0 is muted, 1 is normal.
+                    muted={false}                           // Mutes the audio entirely.
+                    paused={this.state.paused}              
+                    resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
+                    repeat={true}                           // Repeat forever.
+                    playInBackground={false}                // Audio continues to play when app entering background.
+                    playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+                    progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+                    style={[Style.backgroundVideo, {width: width, height: height}]} />
+                    <View style={Style.controls}>
+                        <TouchableOpacity style={Style.playButton} onPress={this._onPlayPress.bind(this)}>
+                            <Image
+                                style={Style.playImage}
+                                source={this.state.playpause_imageuri}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                
+            </View>
+        </BackgroundImage>
        )
+  }
+
+  _onPlayPress() {
+      this.setState({autoplay: true, paused: !this.state.paused})
+      if (this.state.paused) {
+          this.setState({playpause_imageuri: PauseIcon});
+      }
+      else {
+          this.setState({playpause_imageuri: PlayIcon});
+      }
   }
 }
 
