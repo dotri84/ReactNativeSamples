9a1868b710c4b93dd072be45f0bff2c1015689dc
Do Tri <tri@ezyplanet.com>
2017-04-20 09:27:15 +0700

    add settings width height for video

---
+++ VideoPlayerControls/src/Style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+var Style = StyleSheet.create({
+    backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: 100,
+    right: 0,
+  }
+});
+
+export default Style;
